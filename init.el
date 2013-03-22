165a40354509ac84be77c340e4219dd26a3df5ce
Nada Amin <namin@alum.mit.edu>
2013-03-22 10:04:23 +0100

    custom tweaks

--- init.el
+++ init.el
@@ -35,7 +35,18 @@
 (global-undo-tree-mode t)
 
 (set-default-font "Droid Sans Mono 14")
-
-(custom-set-variables '(scheme-program-name "petite"))
-
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(scheme-program-name "petite"))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(region ((t (:background "white smoke")))))
