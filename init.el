ff9f570daf8e8caaac3ae770b7fb6772f3c8251d
Nada Amin <namin@alum.mit.edu>
2013-06-15 08:04:14 -0700

    remove excessive ad-hoc customization

--- init.el
+++ init.el
@@ -37,7 +37,6 @@
 
 (global-undo-tree-mode t)
 
-(set-default-font "Inconsolata 16")
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.scala\\'" . scala-mode))
 
@@ -48,12 +47,3 @@
  ;; If there is more than one, they won't work right.
  '(scheme-program-name "petite"))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(region ((t (:background "white smoke")))))
-
-(load-file "~/.racket/planet/300/5.3.3/cache/neil/scribble-emacs.plt/1/2/scribble.el")
-(require 'scribble)
