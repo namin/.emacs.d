fa602754b02576185e8c168caf74ff44ceca29dc
Nada Amin <nada.amin@epfl.ch>
2014-01-07 13:51:22 +0100

    mac os x routine

--- init.el
+++ init.el
@@ -10,6 +10,7 @@
                       starter-kit-eshell
                       exec-path-from-shell
                       clojure-mode clojure-test-mode nrepl
+                      lua-mode
                       scheme-complete
                       scala-mode
                       markdown-mode
@@ -38,7 +39,8 @@
 (setq-default tab-width 2)
 ;(setq-default c-basic-indent 2)
 ;(setq-default lisp-indent-offset 2)
-(setq visible-bell nil)
+(setq visible-bell t)
+(setq ring-bell-function 'ignore)
 
 (global-undo-tree-mode t)
 
@@ -46,6 +48,7 @@
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.scala\\'" . scala-mode))
 (add-to-list 'auto-mode-alist '("\\.dot\\'" . scala-mode))
+(add-to-list 'auto-mode-alist '("\\.sbt\\'" . scala-mode))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
