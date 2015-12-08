ffefda20c5f62f9a27657cd413ac35c9921f692a
Nada Amin <nada.amin@epfl.ch>
2015-12-08 14:58:58 +0100

    Revert "routine"
    This reverts commit 2e5b5d91e338fdea53b7dba03047f5ae5e842bcd.

--- init.el
+++ init.el
@@ -237,28 +237,28 @@
 
 (global-set-key (kbd "C-x a") 'revert-buffer-no-confirm)
 
-;; ;; OCaml
+;; OCaml
 
-;; ;; Add opam emacs directory to the load-path
-;; (when (executable-find "opam")
-;; (setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
-;; (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+;; Add opam emacs directory to the load-path
+(when (executable-find "opam")
+(setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
+(add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
 
-;; ;; Load merlin-mode
-;; (require 'merlin)
+;; Load merlin-mode
+(require 'merlin)
 
-;; ;; Start merlin on ocaml files
-;; (add-hook 'tuareg-mode-hook 'merlin-mode t)
-;; (add-hook 'caml-mode-hook 'merlin-mode t)
+;; Start merlin on ocaml files
+(add-hook 'tuareg-mode-hook 'merlin-mode t)
+(add-hook 'caml-mode-hook 'merlin-mode t)
 
-;; ;; Enable auto-complete
-;; (setq merlin-use-auto-complete-mode 'easy)
+;; Enable auto-complete
+(setq merlin-use-auto-complete-mode 'easy)
 
-;; ;; Use opam switch to lookup ocamlmerlin binary
-;; (setq merlin-command 'opam)
+;; Use opam switch to lookup ocamlmerlin binary
+(setq merlin-command 'opam)
 
-;; (require 'ocp-indent)
-;; )
+(require 'ocp-indent)
+)
 
 ;; quick hack to run a command on each save of a file
 ;; adapted from http://rtime.felk.cvut.cz/~sojka/blog/compile-on-save/
