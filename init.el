1076e74b7b79bebb82c13ccce34706278e49ac6b
Nada Amin <namin@seas.harvard.edu>
2023-05-31 21:09:39 -0400

    whitespace cleanup

--- init.el
+++ init.el
@@ -44,7 +44,7 @@
 ;; From https://stackoverflow.com/questions/9435019/how-do-i-source-my-zshrc-within-emacs
 (let ((path (shell-command-to-string ". ~/.zshrc; echo -n $PATH")))
   (setenv "PATH" path)
-  (setq exec-path 
+  (setq exec-path
         (append
          (split-string-and-unquote path ":")
          exec-path)))
@@ -270,7 +270,7 @@
 
 (setq bluespec-home "/Users/namin/code/blu/bsc/")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; for bsv mode 
+;; for bsv mode
 (add-to-list 'load-path (concat bluespec-home "/util/emacs"))
 (load "bsv-mode/bsv-mode-23")
 
