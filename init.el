a129a217ddf26f58ecffea287b8baf839078bab1
Nada Amin <namin@seas.harvard.edu>
2024-06-12 17:03:32 -0400

    init from black

---
+++ init.el
@@ -0,0 +1,38 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(jinja2-mode typescript-mode markdown-mode conda highlight-parentheses company rainbow-delimiters paredit undo-tree)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(setq-default frame-title-format '("%b"))
+
+(setq undo-tree-auto-save-history nil)
+
+(setq-default indent-tabs-mode nil)
+(setq tab-width 4)
+
+(global-undo-tree-mode)
+
+(add-to-list 'auto-mode-alist '("\\.dcj\\'" . java-mode))
+
+(add-to-list 'auto-mode-alist '("\\.rkt\\'" . scheme-mode))
+(add-to-list 'auto-mode-alist '("\\.lurk\\'" . scheme-mode))
+(add-to-list 'auto-mode-alist '("\\.smt\\'" . scheme-mode))
+(add-to-list 'auto-mode-alist '("\\.sc\\'" . scheme-mode))
+
+(autoload 'enable-paredit-mode "paredit"
+  "Turn on pseudo-structural editing of Lisp code."
+  t)
+(add-hook 'emacs-lisp-mode-hook       'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           'enable-paredit-mode)
